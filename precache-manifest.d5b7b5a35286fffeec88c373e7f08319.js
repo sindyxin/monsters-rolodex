db2ff9094ed493a91ff6c0c699afe225787b5ed8
anna <sindyxin1@gmail.com>
2020-03-02 00:13:17 -0800

    Updates

---
+++ precache-manifest.d5b7b5a35286fffeec88c373e7f08319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce95b8bd5831dd933843129cfdcf3e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "877d281dff46f10f8e2f",
+    "url": "/monsters-rolodex/static/css/main.e07c1fad.chunk.css"
+  },
+  {
+    "revision": "ee923ba5072250d2912b",
+    "url": "/monsters-rolodex/static/js/2.3387cc9c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.3387cc9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877d281dff46f10f8e2f",
+    "url": "/monsters-rolodex/static/js/main.8ae04aab.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
