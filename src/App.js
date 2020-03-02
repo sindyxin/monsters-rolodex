72d752c27fa56e6c810b298e97a832043106cf36
anna <sindyxin1@gmail.com>
2020-03-02 00:06:51 -0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { CardList } from './components/card-list/card-list.jsx';
+import { SearchBox } from './components/search-box/search-box.jsx';
+// import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [monsters, setMonsters] = useState([]);
+	const [searchField, setSearchField] = useState('');
+	useEffect(() => {
+		axios
+			.get('https://jsonplaceholder.typicode.com/users')
+			.then(response => setMonsters(response.data));
+	}, []);
+
+	const filteredMonsters = monsters.filter(monster =>
+		monster.name.toLowerCase().includes(searchField.toLowerCase())
+	);
+	return (
+		<div className='App'>
+			<h1 className='page-title'>Monsters Rolodex</h1>
+			<SearchBox
+				placeholder='Search Monsters'
+				handleChange={e => setSearchField(e.target.value)}
+			></SearchBox>
+			{/* pass monster as prop from app */}
+			<CardList monsters={filteredMonsters}></CardList>
+		</div>
+	);
 }
 
 export default App;
